967ea3c76d5abb5751eb994f272de8cff5fb5d8b
sling254 <mwanikidenis7@gmail.com>
2021-12-03 23:53:53 +0300

    add login view functionality

--- insta/urls.py
+++ insta/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import index, login, register
+from .views import index, loginView, register
 
 urlpatterns = [
     path('', index,name="index"),
-    path('login/', login,name="login"),
+    path('login/', loginView,name="login"),
     path('register/', register,name="register"),
 ]
--- insta/views.py
+++ insta/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from .forms import CreateUserForm
 
 # Create your views here.
@@ -15,7 +16,17 @@ def index(request):
     return render(request,'base.html', context)
 
 
-def login(request):
+def loginView(request):
+    if request.method == "POST":
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('index')
+        else:
+            messages.info(request,'Username or password is in correct')
 
     return render(request,'auth/login.html')
 
