2bebb0ca9c92ed2bbb1d378ff5c624da31c3bec0
sling254 <mwanikidenis7@gmail.com>
2021-12-04 00:23:37 +0300

    add user logout functionality

--- insta/urls.py
+++ insta/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, loginView, register
+from .views import index, loginView, register, logoutuser
 
 urlpatterns = [
     path('', index,name="index"),
     path('login/', loginView,name="login"),
     path('register/', register,name="register"),
+    path('logout/', logoutuser,name="logout"),
 ]
--- insta/views.py
+++ insta/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from .forms import CreateUserForm
 
 # Create your views here.
 
-
+@login_required(login_url='login')
 def index(request):
     title = "i was in June"
     context={
@@ -17,30 +18,41 @@ def index(request):
 
 
 def loginView(request):
-    if request.method == "POST":
-        username=request.POST.get('username')
-        password=request.POST.get('password')
+    if request.user.is_authenticated:
+        return redirect('index')
+    else:
+        if request.method == "POST":
+            username=request.POST.get('username')
+            password=request.POST.get('password')
 
-        user=authenticate(request,username=username,password=password)
-        if user is not None:
-            login(request,user)
-            return redirect('index')
-        else:
-            messages.info(request,'Username or password is in correct')
+            user=authenticate(request,username=username,password=password)
+            if user is not None:
+                login(request,user)
+                return redirect('index')
+            else:
+                messages.info(request,'Username or password is in correct')
 
-    return render(request,'auth/login.html')
+        return render(request,'auth/login.html')
 
 def register(request):
-    form = CreateUserForm()
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            user = form.cleaned_data.get('username')
-            messages.success(request,'Account created for' + user ) 
-            return redirect('login')
-    context={
-        "form":form
-    }
+    if request.user.is_authenticated:
+        return redirect('index')
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request,'Account created for' + user ) 
+                return redirect('login')
+        context={
+            "form":form
+        }
 
     return render(request,'auth/register.html',context)
+
+
+def logoutuser(request):
+    logout(request)
+    return redirect ('login')
