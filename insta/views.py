d35f56dce3a52f445d5975a6a5902e02758a4e9e
sling254 <mwanikidenis7@gmail.com>
2021-12-03 16:17:28 +0300

    add url configurations for insta app

--- insta/views.py
+++ insta/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def index():
+def index(request):
     title = "i was in June"
     context={
         "title":title,
     }
 
-    return render('base.html', context)
+    return render(request,'base.html', context)
