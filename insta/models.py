d876f392ba8a544edf1caf351057d781e5ab654c
sling254 <mwanikidenis7@gmail.com>
2021-12-04 19:53:04 +0300

    add gitignore

--- insta/models.py
+++ insta/models.py
@@ -8,14 +8,46 @@ from django.utils import timezone
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
-    bio = models.TextField()
-    profile_pic = models.ImageField(null=True, blank=True,upload_to="static/img/profile")
-    website = models.CharField( max_length=50, null=True,blank=True)
+  profile_photo = CloudinaryField('image')
+  bio = models.TextField()
+  user = models.OneToOneField(User,on_delete = models.CASCADE)
 
 
-    def __str__(self):
-        return str(self.user)
+  @receiver(post_save , sender = User)
+  def create_profile(instance,sender,created,**kwargs):
+    if created:
+      Profile.objects.create(user = instance)
+
+  @receiver(post_save,sender = User)
+  def save_profile(sender,instance,**kwargs):
+    instance.profile.save()
+
+  @property
+  def saved_followers(self):
+    return self.followers.count()   
+
+  @property
+  def saved_following(self):
+    return self.following.count() 
+
+
+  @property
+  def follows(self):
+    return [follow.followee for follow in self.following.all()]
+
+  @property
+  def following(self):
+    return self.followers.all()
+
+
+
+  @classmethod
+  def search_profiles(cls,search_term):
+    profiles = cls.objects.filter(user__username__icontains = search_term).all()
+    return profiles
+
+  def __str__(self):
+    return "%s profile" % self.user
 
 
 
