d0c583fcc72e57ad788b21f6c0881c6e797624b5
sling254 <mwanikidenis7@gmail.com>
2021-12-04 19:49:46 +0300

    change the post model

--- insta/models.py
+++ insta/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.http import request
+from django.utils import timezone
 
 # Create your models here.
 
@@ -18,12 +20,37 @@ class Profile(models.Model):
 
 
 class Post(models.Model):
-    image = CloudinaryField('image')
-    photo_name = models.CharField(max_length=60)
-    posted_at = models.DateTimeField(auto_now_add=True)
-    photo_caption = models.TextField()
-    user = models.ForeignKey(User,on_delete = models.CASCADE)
+  photo = CloudinaryField('image')
+  photo_name = models.CharField(max_length=60)
+  posted_at = models.DateTimeField(auto_now_add=True)
+  photo_caption = models.TextField()
+  user = models.ForeignKey(User,on_delete = models.CASCADE)
 
-    def save_photo(self):
-        self.save()
+  def save_photo(self):
+     self.save()
+
+  @classmethod
+  def display_photos(cls):
+    photos = cls.objects.all().order_by('-posted_at')
+    return photos
+
+  @property
+  def saved_comments(self):
+    return self.comments.all()
+
+  @property
+  def saved_likes(self):
+    return self.photolikes.count()
+
+  @classmethod
+  def search_photos(cls,search_term):
+    photos = cls.objects.filter(photo_name__icontains = search_term).all()
+    return photos
+
+  def delete_post(self):
+    self.delete()
+
+  def __str__(self):
+    return "%s photo" % self.photo_name
+
 
