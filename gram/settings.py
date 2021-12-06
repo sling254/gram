24d2b47106c4be82901200311a9affa38787592e
sling254 <mwanikidenis7@gmail.com>
2021-12-06 12:01:11 +0300

    configure app to send emails

--- gram/settings.py
+++ gram/settings.py
@@ -124,7 +124,12 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
+# Email configurations 
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
